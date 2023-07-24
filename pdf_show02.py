91dcddc20fbe60bde55a134db94360a42fcd55f7
mitsonagaoka <mmaro.mitsu825@gmail.com>
2023-07-24 19:02:51 +0900

    requirements　を追加する

--- pdf_show02.py
+++ pdf_show02.py
@@ -1,18 +1,11 @@
-import streamlit as st
 import base64
 
-def main():
-    st.title("PDF Viewer")
-    pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+# 文字列をバイト列にエンコードしてBase64エンコード
+data = "Hello, world!"
+encoded_data = base64.b64encode(data.encode("utf-8")).decode("utf-8")
 
-    if pdf_file is not None:
-        # PDFファイルをBase64エンコードしてHTMLタグに埋め込む
-        pdf_base64 = base64.b64encode(pdf_file.read()).decode("utf-8")
-        pdf_embed = f'<embed src="data:application/pdf;base64,{pdf_base64}" width="100%" height="600px" type="application/pdf">'
-        st.markdown(pdf_embed, unsafe_allow_html=True)
+# Base64エンコードされたデータをデコード
+decoded_data = base64.b64decode(encoded_data).decode("utf-8")
 
-if __name__ == "__main__":
-    main()
-
-
-# streamlit run pdf_show02.py
+print("Encoded:", encoded_data)
+print("Decoded:", decoded_data)
