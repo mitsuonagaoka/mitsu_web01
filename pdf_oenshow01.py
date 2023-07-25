b1b1e028d7f7e64838ad8f3b5d89afdc03c1f4a7
mitsonagaoka <mmaro.mitsu825@gmail.com>
2023-07-25 11:17:32 +0900

    third commit

--- pdf_oenshow01.py
+++ pdf_oenshow01.py
@@ -1,28 +1,37 @@
 import streamlit as st
-import base64
+from pdf2image import convert_from_path
+import tempfile
 import os
 
+
 def invoice_show44():
-    st.title("PDF Viewer")
-    pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+    st.title("PDFファイルビューア")
 
-    if pdf_file is not None:
-        # 一時的なファイルにPDFデータを書き込む
-        with open("temp.pdf", "wb") as f:
-            f.write(pdf_file.read())
+    uploaded_file = st.file_uploader("PDFファイルをアップロードしてください", type=["pdf"])
 
-        # 一時的なファイルをBase64エンコードしてHTMLタグに埋め込む
-        with open("temp.pdf", "rb") as f:
-            pdf_base64 = base64.b64encode(f.read()).decode("utf-8")
-            pdf_embed = f'<embed src="data:application/pdf;base64,{pdf_base64}" width="100%" height="600px" type="application/pdf">'
-            st.markdown(pdf_embed, unsafe_allow_html=True)
+    if uploaded_file:
+        # 一時ファイルをディスクに保存してからパスを取得
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(uploaded_file.read())
+            temp_file_path = temp_file.name
 
-        # 一時的なファイルを削除
-        os.remove("temp.pdf")
+        file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type, "FileSize": uploaded_file.size}
+        st.write(file_details)
 
-        st.run(port=8502)  # ポート番号を8502に変更
+        # PDFを画像に変換
+        images = convert_from_path(temp_file_path)
+
+        # 画像を表示
+        for image in images:
+            st.image(image, use_column_width=True)
+
+        # 一時ファイルを削除
+        os.remove(temp_file_path)
 
 invoice_show44()
 
-# streamlit run pdf_oenshow01.py
+
+# if __name__ == "__main__":
+#     main()
+
 # streamlit run pdf_oenshow01.py
--- pdf_show02.py
+++ pdf_show02.py
@@ -1,11 +1,35 @@
-import base64
+import streamlit as st
+from pdf2image import convert_from_path
+import tempfile
+import os
 
-# 文字列をバイト列にエンコードしてBase64エンコード
-data = "Hello, world!"
-encoded_data = base64.b64encode(data.encode("utf-8")).decode("utf-8")
 
-# Base64エンコードされたデータをデコード
-decoded_data = base64.b64decode(encoded_data).decode("utf-8")
+def main():
+    st.title("PDFファイルビューア")
 
-print("Encoded:", encoded_data)
-print("Decoded:", decoded_data)
+    uploaded_file = st.file_uploader("PDFファイルをアップロードしてください", type=["pdf"])
+
+    if uploaded_file:
+        # 一時ファイルをディスクに保存してからパスを取得
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(uploaded_file.read())
+            temp_file_path = temp_file.name
+
+        file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type, "FileSize": uploaded_file.size}
+        st.write(file_details)
+
+        # PDFを画像に変換
+        images = convert_from_path(temp_file_path)
+
+        # 画像を表示
+        for image in images:
+            st.image(image, use_column_width=True)
+
+        # 一時ファイルを削除
+        os.remove(temp_file_path)
+
+
+if __name__ == "__main__":
+    main()
+
+# streamlit run pdf_show02.py
